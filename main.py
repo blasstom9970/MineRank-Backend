ed45811333afac1fecb7843d98c0dd0d25108e1f
rrayy <72653431+rrayy-25809@users.noreply.github.com>
2025-10-28 14:42:40 +0900

    Changes to be committed: 제바류ㅠㅠ 	new file:   .github/workflows/deploy.yml 	modified:   main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ load_dotenv()
 # Flask 설정
 app = Flask(__name__)
 app.secret_key = os.getenv("FLASK_SECRET_KEY")
-CORS(app)  # 모든 출처 허용. 필요한 경우 특정 도메인만 허용 가능
+CORS(app, resources={r"/*": {"origins": "https://mine-rank.netlify.app"}}) # 마인랭크 클라만 허용
 
 # DuckDB 설정
 db_path:str = os.getenv("DUCKDB_PATH") # type: ignore
