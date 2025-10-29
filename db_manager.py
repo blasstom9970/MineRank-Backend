3261649c9780e9f8f85517999d3141efbd12a232
김민결 <139607260+tatatommy6@users.noreply.github.com>
2025-10-29 21:07:36 +0900

    update add_entry()
    sql 인젝션 방지 코드 및 예외처리 코드 추가

--- db_manager.py
+++ db_manager.py
@@ -20,12 +20,18 @@ class DataBaseManager:
         ''') 
 
     def add_entry(self, table_name: str, data:dict):
+        # 빈 data면 예외처리 
+        if not data:
+            raise ValueError("add_entry: 'data'가 비어있습니다.")
+        
         cursor = self.cursor # type: ignore
+        # 원래는 문자열 그대로 넣었지만 컬럼명/테이블명 만 f-string으로 넣어줌 -> 인젝션 공격 방지
+        columns = ', '.join([f'"{k}"' for k in data.keys()])
+        
+        # 값을 직접 넣지 않고 '?'플레이스홀더로 바꿈
+        placeholders = ', '.join(['?'] * len(data))
 
-        columns = ', '.join(data.keys())
-        values = ', '.join([f"'{v}'" for v in data.values()])
-
-        cursor.execute(f'''
-            INSERT INTO {table_name} ({columns})
-            VALUES ({values});
-        ''')
+        # 테이블/컬럼은 포맷, 값은 파라미터로 전달하여 SQL 인젝션 방지
+        #f-string보다 테이블 이름을 ""로 감싸주면서 안정성을 높임
+        cursor.execute(f'INSERT INTO "{table_name}" 
+        ({columns})VALUES ({placeholders});',tuple(data.values()))
